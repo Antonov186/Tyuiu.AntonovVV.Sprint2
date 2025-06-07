58727d5fbd5ff95cb25987afe108ad7fb2f6fec4
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 13:52:21 +0500

    Task4

---
+++ Tyuiu.AntonovVV.Sprint2.Task4.V7.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.AntonovVV.Sprint2.Task4.V7.Lib;
+namespace Tyuiu.AntonovVV.Sprint2.Task4.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
