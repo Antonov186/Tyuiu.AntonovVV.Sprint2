4f2b6ac2952af928dd66daa3c251b17508269bdc
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 14:05:28 +0500

    Task6

---
+++ Tyuiu.AntonovVV.Sprint2.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AntonovVV.Sprint2.Task6.V14.Lib;
+namespace Tyuiu.AntonovVV.Sprint2.Task6.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+
+    {
+
+        [TestMethod]
+
+        public void ValidFindDayName()
+
+        {
+
+            DataService ds = new DataService();
+
+            int k = 15;
+
+            int d = 7;
+
+            string res = ds.FindDayName(k, d);
+
+            string wait = "Âîñêðåñåíüå";
+
+            Assert.IsTrue(res == wait);
+
+
+
+
+
+        }
+
+    }
+
+}
