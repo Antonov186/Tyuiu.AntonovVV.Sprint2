bd57422fa199162d046938d87253e98b2f9ad017
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 13:37:40 +0500

    Task2

---
+++ Tyuiu.AntonovVV.Sprint2.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AntonovVV.Sprint2.Task2.V27.Lib;
+namespace Tyuiu.AntonovVV.Sprint2.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
